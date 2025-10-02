023a0f5a4c7af60dbe69aa2d28ff5c3a474e0d70
FrancoBarra <122551451+FrancoBarra@users.noreply.github.com>
2025-10-02 15:10:29 -0400

    inicio

--- src/proyecto1_franco_barra_roger_balan/Proyecto1_Franco_Barra_Roger_Balan.java
+++ src/proyecto1_franco_barra_roger_balan/Proyecto1_Franco_Barra_Roger_Balan.java
@@ -15,7 +15,7 @@ public class Proyecto1_Franco_Barra_Roger_Balan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola git");
     }
     
 }
