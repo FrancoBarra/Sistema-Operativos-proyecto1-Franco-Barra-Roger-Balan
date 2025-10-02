fa1de77471db518414e1ab283a8db182bb94290e
FrancoBarra <122551451+FrancoBarra@users.noreply.github.com>
2025-10-02 15:04:02 -0400

    creación

---
+++ src/proyecto1_franco_barra_roger_balan/Proyecto1_Franco_Barra_Roger_Balan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_franco_barra_roger_balan;
+
+/**
+ *
+ * @author frank
+ */
+public class Proyecto1_Franco_Barra_Roger_Balan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
